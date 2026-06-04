094983c66aa2cbca48bc06fc98f607a7d6660878
erjemin <erjemin@gmail.com>
2026-06-04 15:34:23 +0300

    add: приложение frontend и настройки filer

--- lpon_site/frontend/apps.py
+++ lpon_site/frontend/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class FrontendConfig(AppConfig):
     name = 'frontend'
+    verbose_name = 'Сайт lpon.ru'
+    # Переключаем на стандартный AutoField (до 2 млрд записей)
+    default_auto_field = 'django.db.models.AutoField'
+
+    # def ready(self):
+        ## Импортируем сигналы при запуске приложения
+        # import myapp.signals
