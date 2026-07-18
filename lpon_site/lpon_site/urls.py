d99c59718e5d348a8dcdd4932b83a2d769bd536f
erjemin <erjemin@gmail.com>
2026-07-19 02:00:19 +0300

    add: front (1) Сплеш-скрин index

--- lpon_site/lpon_site/urls.py
+++ lpon_site/lpon_site/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+
+from frontend import views
 
 urlpatterns = [
     # Админ-сайт с переименованными приложениями (переопределен в frontend/apps.py)
     path(settings.ADMIN_URL, admin.site.urls),
+
+    re_path(r'^$', views.index),
 ]
 
 # Сервировать медиа-файлы в разработке (в production использовать nginx/Apache)
